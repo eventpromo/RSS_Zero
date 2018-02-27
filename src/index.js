932e9c6b848ac3c16e4b4658aa231fafb4c71ed4
MINSK\Siarhei_Huk <eventpromoby@gmail.com>
2018-02-27 17:52:40 +0300

    Implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var result = 0;
+  var div = 5;
+  while(number > div){
+    result += Math.floor(number/div)
+    div *= 5;
+  }
+  return result;
 }
